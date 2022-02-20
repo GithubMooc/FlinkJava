d8d5f9fb4e4c2598b1679acd14c33759659a312d
alipay <alipay@aliyun.com>
2022-02-20 19:10:45 +0800

    Initial Commit

--- src/main/java/com/day02/source/Demo01.java
+++ src/main/java/com/day02/source/Demo01.java
@@ -10,10 +10,11 @@ import java.util.*;
  * @Date 2022/2/9
  * @Time 22:45
  * @Name FlinkJava
+ *
+ * 创建不同类型的执行环境
+ *
+ * 从Java的集合中读取数据
  */
-/*
-* 从Java的集合中读取数据
-*/
 public class Demo01 {
     public static void main(String[] args) throws Exception {
         List<WaterSensor> waterSensors = Arrays.asList(
@@ -21,6 +22,14 @@ public class Demo01 {
                 new WaterSensor("ws_002", 1577844015L, 43),
                 new WaterSensor("ws_003", 1577844020L, 42));
 
+
+        // 返回本地执行环境， 需要在调用时指定默认的并行度。
+        // LocalStreamEnvironment localEnvironment = StreamExecutionEnvironment.createLocalEnvironment(1);
+        // LocalEnvironment localEnvironment1 = ExecutionEnvironment.createLocalEnvironment(1);
+
+        // 返回集群执行环境， 将 Jar 提交到远程服务器。 需要在调用时指定 JobManager 的 IP 和端口号， 并指定要在集群中运行的 Jar 包。
+        // ExecutionEnvironment localhost = ExecutionEnvironment.createRemoteEnvironment("localhost", 9999, "YOURPATH//wordcount.jar");
+        // StreamExecutionEnvironment localhost1 = StreamExecutionEnvironment.createRemoteEnvironment("localhost", 9999, "YOURPATH//wordcount.jar");
         // 1. 创建执行环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env
